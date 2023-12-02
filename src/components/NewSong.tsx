59edc9f02a9d8d834ed5186f3ebbb1c7a630838a
Pedro Ángel Serrano Rodrigues <pedroangelserrano@hotmail.es>
2023-12-02 19:31:37 +0100

    Proyecto casi terminado solo comentar y desplazamiento canciones

--- src/components/NewSong.tsx
+++ src/components/NewSong.tsx
@@ -3,30 +3,31 @@ import { NewSongProps } from "../hooks/Types";
 
 const NewSong: React.FC<NewSongProps> = ({ onNewInput }) => {
   const inputText = useRef<HTMLInputElement>(null);
-    return(
-        <input 
-          placeholder="ej: Canción(Artista)"
-          className="inputTarea"
-          type="text"
-          ref={inputText}
-            onKeyUp={(event) => {
-              if (event.key =='Enter' && inputText.current!.value.length>0){
-                //users.push("Nuevo")
-                //setUsers([...users, 'nuevoooo'])//IMPORTANTE PARA MODIFICAR ARRAYS DINÁMICAMENTE
-                onNewInput({
-                  id: crypto.randomUUID(),
-                  listen: false,
-                  text: inputText.current!.value,
-                });
-                inputText.current!.value = ''
-                console.log("INTRO DADO");
-              }
-            }
-          }
-        />
-    )
+
+  const handleKeyUp = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && inputText.current!.value.length > 0) {
+      onNewInput({
+        id: crypto.randomUUID(),
+        listen: false,
+        text: inputText.current!.value,
+      });
+      inputText.current!.value = '';
+      console.log("INTRO DADO");
     }
-    
-    export default NewSong
-    
-    
+  };
+
+  return (
+    <div className='center-container'>
+        <span className='tituloInTarea'>Introduce una canción: </span>
+    <input
+      placeholder="ej: Canción(Artista)"
+      className="inputTarea"
+      type="text"
+      ref={inputText}
+      onKeyUp={handleKeyUp}
+    />
+    </div>
+  );
+};
+
+export default NewSong;
